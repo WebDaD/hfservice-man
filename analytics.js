4b999464593304026f119f247d3edc647848ba12
Domink Sigmund <sigmund.dominik@googlemail.com>
2018-05-22 09:51:18 +0200

    datalayer problem

--- analytics.js
+++ analytics.js
@@ -1,29 +1,29 @@
-/* global dataLayer */
-var audios = document.getElementsByClassName('accordion')
-
-for (var i = 0; i < audios.length; i++) {
-  audios[i].addEventListener('play', function () {
-    dataLayer.push({
-      event: 'audio',
-      eventCategory: 'radio',
-      eventAction: 'play',
-      eventLabel: this.dataset.title
+/* global $, dataLayer */
+$(document).ready(function () {
+  $('audio').each(function (index) {
+    $(this).on('play', function () {
+      dataLayer.push({
+        event: 'audio',
+        eventCategory: 'radio',
+        eventAction: 'play',
+        eventLabel: $(this).data('title')
+      })
+    })
+    $(this).on('pause', function () {
+      dataLayer.push({
+        event: 'audio',
+        eventCategory: 'radio',
+        eventAction: 'pause',
+        eventLabel: $(this).data('title')
+      })
+    })
+    $(this).on('stalled', function () {
+      dataLayer.push({
+        event: 'audio',
+        eventCategory: 'radio',
+        eventAction: 'stalled',
+        eventLabel: $(this).data('title')
+      })
     })
   })
-  audios[i].addEventListener('pause', function () {
-    dataLayer.push({
-      event: 'audio',
-      eventCategory: 'radio',
-      eventAction: 'pause',
-      eventLabel: this.dataset.title
-    })
-  })
-  audios[i].addEventListener('stalled', function () {
-    dataLayer.push({
-      event: 'audio',
-      eventCategory: 'radio',
-      eventAction: 'download',
-      eventLabel: this.dataset.title
-    })
-  })
-}
+})
